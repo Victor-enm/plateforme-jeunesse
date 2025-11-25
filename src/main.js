22549705f16d3d8628eb214ea29caf5a768c10f3
PRUDHON Victor <victor.prudhon@enm.justice.fr>
2025-11-25 09:26:44 +0100

    correction matomo

--- src/main.js
+++ src/main.js
@@ -1,22 +1,20 @@
 import { createApp } from 'vue'
-import './assets/tailwind.css'
-import VueMatomo from 'vue-matomo'
 import App from './App.vue'
 import router from './router'
+import VueMatomo from 'vue-matomo'
+import './assets/tailwind.css'
 import './assets/fonts.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
+const app = createApp(App)
 
-Vue.use(VueMatomo, {
+app.use(router)
+
+app.use(VueMatomo, {
   host: 'https://enm.matomo.cloud/',
-  siteId: 9
-});
-
-new Vue({
-  el: '#app',
-  router,
-  components: {App},
-  template: ''
+  siteId: 9,
+  router, // permet de tracker les changements de routes automatiquement
+  enableLinkTracking: true
 })
 
-window._paq.push(['trackPageView']); // Pour suivre les visites sur vos pages
+app.mount('#app')
