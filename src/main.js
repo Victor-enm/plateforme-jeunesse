4c5b66622ded5a909127297a753d24d42e37ee25
PRUDHON Victor <victor.prudhon@enm.justice.fr>
2025-11-25 09:11:43 +0100

    ajout matomo - first test

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import { createApp } from 'vue'
 import './assets/tailwind.css'
+import VueMatomo from 'vue-matomo'
 import App from './App.vue'
 import router from './router'
 import './assets/fonts.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 
-createApp(App).use(router).mount('#app')
+Vue.use(VueMatomo, {
+  host: 'https://enm.matomo.cloud/',
+  siteId: 9
+});
+
+new Vue({
+  el: '#app',
+  router,
+  components: {App},
+  template: ''
+})
+
+window._paq.push(['trackPageView']); // Pour suivre les visites sur vos pages
