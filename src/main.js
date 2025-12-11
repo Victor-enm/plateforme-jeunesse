de43e51863dd0f65a284b5a66733d0705e62c804
PRUDHON Victor <victor.prudhon@enm.justice.fr>
2025-12-11 14:32:48 +0100

    patch correctif matomo - refacto in prod

--- src/main.js
+++ src/main.js
@@ -1,20 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueMatomo from 'vue-matomo'
 import './assets/tailwind.css'
 import './assets/fonts.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.use(VueMatomo, {
-  host: 'https://enm.matomo.cloud/',
-  siteId: 9,
-  router, // permet de tracker les changements de routes automatiquement
-  enableLinkTracking: true
-})
-
-app.mount('#app')
+createApp(App).use(router).mount('#app');
